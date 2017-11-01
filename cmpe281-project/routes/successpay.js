a2552e17915542d1b835ec751651034579c9429f
anjanap <anjanap1308@gmail.com>
2017-10-31 19:02:11 -0700

    add to db

--- cmpe281-project/routes/successpay.js
+++ cmpe281-project/routes/successpay.js
@@ -3,18 +3,18 @@ var mongo = require("./mongo");
 var mongoURL = "mongodb://localhost:27017/project281";
 
 function updatestatus(req,res){
+	var mode=req.param("mode");
+	var cnumber=req.param("cardno");
+	var newbill={user_id: 1,mode:mode,card_num:cnumber};
+console.log(mode+cnumber);
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
-        var col1 = mongo.collection('cart_details');
-        
-        col1.findOne({user_id: 1}, function(err, cart){
-            if (cart) {
-           	console.log("USER: "+cart.total);
-           	res.render('successpay.ejs',{ total: cart.total });
-            } else {
-            	res.render('error.ejs');
-            }
-        });
+        var col1 = mongo.collection('payment');
+        col1.insertOne(newbill, function(err, rest) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            res.render('successpay.ejs');
+            });
 
     });  
 }
