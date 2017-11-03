ae9b344e01ff62b60d633363e44fdb397226a4f3
Varun Upadhyay <varun.u28@gmail.com>
2017-11-03 12:36:20 -0700

    Update category.js

--- cmpe281-project/routes/category.js
+++ cmpe281-project/routes/category.js
@@ -6,11 +6,13 @@ function check(req,res){
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('category');
+        var order_item_id = 1;
 
+        // Keeping a check to prevent the same item from getting recommended
         coll.findOne({category_id : 1}, function(err, cat){
-            if (cat) {
-           	console.log("Category: "+cat.category_name);
-           	res.render('category.ejs');
+            if (cat && coll.order_item_id != 1) {
+           	   console.log("You may like: " + cat.category_name);
+           	   res.render('category.ejs');
             } else {
             	res.render('error.ejs');
             }
