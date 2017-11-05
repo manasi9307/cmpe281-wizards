43583a226ea4e8a406f74d68bdbf586fbbab72c6
Pooja-Shivasharanappa <31825658+Pooja-Shivasharanappa@users.noreply.github.com>
2017-11-04 19:03:33 -0700

    Insert cart_details into db

--- cmpe281-project/routes/cart.js
+++ cmpe281-project/routes/cart.js
@@ -22,15 +22,35 @@ function cart(req,res){
 }];
 var user_details = [{
   "user_id": 1,
-  "cart_id": 1
+  "cart_id": 1,
+    "status": "open"
 }};
  var total=0;
+// insert the details into the db
+  coll.insert({cart_id: user_details.item, user_id: user_details.user_id,status : user_details.status}, function(err, user){
+         
+        if (user) {
+          console.log('SuCCESS');
+          res.code = '200';
+          res.value = '1';
+          
+        } else {
+          console.log('ERROR');
+          res.code = '401';
+          res.value = '0';
+         
+        }
+      
+    });
+
+
+
+
 //check if the user is prime user and then redirect him to payment page or just display the contents of the particular user
                     coll.findOne({user_details.user_id}),function(err, user){
                     if(user){
                     if(user.flag==1){
-                                     
-for(var i=0;i<cart_items.length;i++){
+                        for(var i=0;i<cart_items.length;i++){
   coll.findOne({product_name: cart_item[i].item}, function(err, user){
             if (user) {
            	//console.log("USER: "+user.name+" College:"+user.college);
@@ -40,12 +60,7 @@ for(var i=0;i<cart_items.length;i++){
             	res.render('error.ejs');
             }
         });
-    });
-
-}
-}else{
- 
-}}
+    });                         
 
 }
 exports.payment=check;
