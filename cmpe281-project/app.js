42f80f44aa8ab14921f503d5e8df7fbeb4aeb63f
Anjana <anjanap1308@gmail.com>
2017-11-09 16:20:40 -0800

    intehration app.js

--- cmpe281-project/app.js
+++ cmpe281-project/app.js
@@ -7,10 +7,13 @@ var express = require('express')
   , routes = require('./routes')
   , payment = require('./routes/payment')
     , success = require('./routes/successpay')
-  ,  menu=require('./routes/menu');
-  ,  cart=require('./routes/cart');
+  ,  menu=require('./routes/menu')
+  ,  login=require('./routes/login')
+    ,  cartid=require('./routes/createcartid')
+  ,  cart=require('./routes/cart')
   , http = require('http')
   , path = require('path');
+  var bodyParser=require('body-parser');
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,10 +35,29 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+
+
+app.post('/', function(req, res) {
+    passport.authenticate('login', function(err, user) {
+        if(!user) {
+        	console.log("CHECK: "+ user);
+        	res.status(201).json({output:0});
+        }
+        else{
+        req.session.user = user.username;
+        console.log("Session initialised: "+req.session.user);
+        //req.session.save();
+        res.status(201).send({output:user.r});}
+
+    })(req, res);
+});
+app.post('/login', login.login);
+app.post('/createcartid', cartid.createcartid);
+/*
 app.post('/payment', payment.payment);
 app.post('/cart', cart.cart);
 app.get('/menu', menu.menus);
-app.post('/successpay', success.success);
+app.post('/successpay', success.success);*/
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
