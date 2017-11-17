7c322872a8cc4b6d05147ba22854868c2d525c28
Varun Upadhyay <varun.u28@gmail.com>
2017-11-16 16:13:29 -0800

    Integrated with menu.js

--- cmpe281-project/routes/category.js
+++ cmpe281-project/routes/category.js
@@ -1,8 +1,11 @@
 var ejs = require("ejs");
 var mongo = require("./mongo");
-var mongoURL = "mongodb://localhost:27017/cmpe281";
+var mongoURL = "mongodb://localhost:27017/project281";
 
-function check(req,res){
+
+function check(req,res,int id){
+console.log(id);
+/*
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('product_catalogue');
@@ -25,4 +28,6 @@ function check(req,res){
             }
         });
     });  
+*/
 }
+
