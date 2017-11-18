9c31d410554b885047966bdbb71d80c19ecfa4f8
Varun Upadhyay <varun.u28@gmail.com>
2017-11-17 16:06:54 -0800

    Update category.js to take the product id from menu.js as a parameter to function call

--- cmpe281-project/routes/category.js
+++ cmpe281-project/routes/category.js
@@ -3,12 +3,11 @@ var mongo = require("./mongo");
 var mongoURL = "mongodb://localhost:27017/project281";
 
 
-function check(req,res,int id){
+function check(req,res,int order_item_id){
 console.log(id);
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('product_catalogue');
-        var order_item_id = 1;
         var categ_id = -1;
         
         // Finding the category id associated to the product id
